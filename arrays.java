27acd9d844fdad58d3361668c407344446631b9a
lakkaramnaveen <lakkaramnaveen4488@gmail.com>
2026-01-20 08:33:07 -0600

    arrays

---
+++ arrays.java
@@ -0,0 +1,13 @@
+class arrays{
+    public static void main(String[] args) {
+        int[] marks = new int[3];
+        marks[0] = 21;
+        marks[1] = 22;
+        marks[2] = 23;
+        for (int i : marks) {
+            System.out.println(i);
+        }
+    }
+}
+
+
